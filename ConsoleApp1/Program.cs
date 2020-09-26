7c5703f68569f334b838cc39a7590142fdd50de0
DnLRdz <71399970+DnLRdz@users.noreply.github.com>
2020-09-26 02:02:39 -0500

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,52 @@
-﻿using System;
+//nmb=nombre
+//app=apellido paterno
+//apm=apellido materno
+//ed=edad
+//ocp=ocupación
+//cor=correo
 
-namespace ConsoleApp1
+
+using System;
+
+
+namespace ConsoleSwap
 {
     class Program
     {
-        static void Main(string[] args)
-        {//
-            string Nombre;
-            string apellidos;
-            int Edad;
-            string Ocupación;
-            string correo;
+        public static void Main(string[] args)
+        {
+            string nmb, app, apm, ocp, eml;
+            int mined;
+            {
+                Console.WriteLine("Ingresa tu nombre(s): ");
+                nmb = Console.ReadLine();
+                Console.WriteLine("Ingresa tu apellido paterno: ");
+                app = Console.ReadLine();
+                Console.WriteLine("Ingresa tu apellido materno: ");
+                apm = Console.ReadLine();
+                Console.WriteLine("¿Cuál es su ocupación?: ");
+                ocp = Console.ReadLine();
+                Console.WriteLine("Escribe tu e-mail: ");
+                eml = Console.ReadLine();
+                Console.WriteLine("Ingresa tu edad: ");
+                mined = Convert.ToInt32(Console.ReadLine());
+                if (mined < 18)
+                {
+                    Console.WriteLine("Acceso restringido a menores de 18 años.");
+                }
+
+                else if (mined >= 18);
+                Console.WriteLine("Por favor, verifique sus datos:");
+                Console.WriteLine("\nSu nombre completo es: {0}  {1}  {2} ", nmb, app, apm);
+                Console.WriteLine("\nSu edad es: {0}",mined);
+                Console.WriteLine("\nSu e-mail es: {0}", eml);
+                Console.WriteLine("\nSu ocupación es: {0}", ocp);
+
+
+            } 
+           
         }
     }
 }
+
+
